beb20a22ede8cf4542712b76054bb8e05fbf547c
Tasmia-jokhio <tasmiajokhio@gmail.com>
2025-06-03 12:16:07 +0500

    newcommit

--- traficlightimplimentation/src/TrafficLightSimulator.jsx
+++ traficlightimplimentation/src/TrafficLightSimulator.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import { useEffect,useRef } from "react";
 
 export const TrafficLightSimulator = () => {
+
+    const {state}=useState();
+    const divforgreen=useRef();
+    function toggleHandlerforgrean(){
+    if(divref.current.style.backgroundColor==="lightslategray"){
+      divref.current.style.backgroundColor="green"
+
+    }
+    else if(divrefred.current.style.backgroundColor==="lightslategray"){
+      divref.current.style.backgroundColor="red"
+
+    }
+
+    else if(divrefyellow.current.style.backgroundColor==="lightslategray"){
+      divref.current.style.backgroundColor="yellow"
+
+    }
+
+    else{
+      divref.current.style.backgroundColor="lightslategray"
+    }
+  }
+
   return (
     <>
       <div
@@ -8,15 +32,15 @@ export const TrafficLightSimulator = () => {
         
       >
         <div
-          className="inner"
+          className="inner" ref={divforgreen}
          
         ></div>
         <div
-          className="inner"
+          className="inner"  ref={divforyellow}
          
         ></div>
         <div
-          className="inner"
+          className="inner" ref={divforred}
         
         ></div>
       </div>
---
+++ traficlightimplimentation/src/trydemo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+
+export const trydemo = () => {
+    
+  return (
+    <div>try</div>
+  )
+}
