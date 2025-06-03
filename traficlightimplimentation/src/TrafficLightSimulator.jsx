259973d3c43e01c0f875f8383f612eeef05b8268
Tasmia-jokhio <tasmiajokhio@gmail.com>
2025-06-03 11:56:05 +0500

    initialcommits

---
+++ traficlightimplimentation/src/TrafficLightSimulator.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const TrafficLightSimulator = () => {
+  return (
+    <>
+      <div
+        className="outer"
+        
+      >
+        <div
+          className="inner"
+         
+        ></div>
+        <div
+          className="inner"
+         
+        ></div>
+        <div
+          className="inner"
+        
+        ></div>
+      </div>
+    </>
+  );
+};
